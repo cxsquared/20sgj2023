0ff654d2a4ed0f7c8f128ede39682be859aff989
Cody Claborn <codylclaborn@gmail.com>
2023-11-11 14:45:08 -0500

    Initial commit

---
+++ src/constant/GameAction.hx
@@ -0,0 +1,18 @@
+package constant;
+
+enum abstract GameAction(Int) to Int {
+	var MoveX;
+	var MoveY;
+
+	var Jump;
+	var Interact;
+	var Pause;
+	var MenuSelect;
+	var MenuDown;
+	var MenuUp;
+	var MenuSelect1;
+	var MenuSelect2;
+	var MenuSelect3;
+	var MenuSelect4;
+	var MenuSelect5;
+}
